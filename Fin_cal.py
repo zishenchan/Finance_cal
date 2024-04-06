1ef03d1b947c180de1f5e2ba20b64c4f44166848
Zishen Chen <zche4727@uni.sydney.edu.au>
2024-04-06 17:22:39 +1100

    17.22 6/4 2024

--- Fin_cal.py
+++ Fin_cal.py
@@ -1,20 +1,26 @@
-'''
-first step will be the interface
+
+
+# create GUI
+import PySimpleGUI as sg
 
 
 
-'''
+# All the stuff inside your window.
+layout = [  [sg.Text('Your Initial Deposit: ')],
+            [sg.Text('Enter Your Compound Times'), sg.InputText()],
+            [sg.Button('Ok'), sg.Button('Cancel')] ]
+
+# Create the Window
+window = sg.Window('Compound Calculator', layout)
 
 
-def cal(){
+# Event Loop to process "events" and get the "values" of the inputs
+while True:
+    event, values = window.read()
+    if event == sg.WIN_CLOSED or event == 'Cancel': # if user closes window or clicks cancel
+        break
+    print('You entered ', values[0])
 
-    # simple calcuation
+window.close()
 
 
-
-    
-
-
-
-
-}
